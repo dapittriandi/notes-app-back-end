d44669c971a72335375e659fcd93d2d5dd1dc02c
DapitTriandi <dpttrndi88@gmail.com>
2023-07-18 20:37:34 +0700

    initial comit

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByidHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handler');
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByidHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    },
+];
+
+module.exports = routes;
